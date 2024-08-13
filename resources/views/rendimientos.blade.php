f24ac29826ef6ca1e5247a5ce54d140b32d8bd77
CARDONA LUIS <kikecar97@gmail.com>
2024-08-13 13:13:27 -0600

    updated routes

---
+++ resources/views/rendimientos.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>rendimientos</h1>
+@stop
+
+@section('content')
+    <p>Pagina principal de rendimientos</p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
